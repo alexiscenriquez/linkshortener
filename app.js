a0a818a9723df6a6900573abdd72ad9453eea946
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-12-30 21:53:37 -0600

    functionality

---
+++ app.js
@@ -0,0 +1,34 @@
+
+let shorten=document.querySelector("#shorten")
+let link=document.querySelector("#link")
+let url
+let form=document.querySelector("form")
+shorten.addEventListener("click",shortenURL)
+
+function shortenURL(){
+    url=document.querySelector("#url").value
+let body= {
+    url: `${url}`,
+domain: `tiny.one`
+}
+
+fetch(`https://api.tinyurl.com/create`, {
+    method: `POST`,
+    headers: {
+      accept: `application/json`,
+      authorization: `Bearer Us82z6SGGKvLuK2vAdWaIvLmSOpk22UJoj1PdDYSjGjgN6mnDjo88tkBedy4 `,
+      'content-type': `application/json`,
+    },
+    body: JSON.stringify(body)
+})
+.then(response => {
+    if (response.status != 200) throw `There was a problem with the fetch operation. Status Code: ${response.status}`;
+    return response.json()
+  })
+  .then(data => {
+    link.textContent=data.data.tiny_url
+  })
+  .catch(error =>
+    { console.log(error)
+    link.textContent="Invalid URL"});
+    }
